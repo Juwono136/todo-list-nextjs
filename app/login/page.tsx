cdbf8211fc327e3a16f4eea83f57f96524caa948
unosega@gmail.com <unosega@gmail.com>
2026-03-02 09:33:54 +0700

    create sign-up feature

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -9,6 +9,7 @@ import { Input } from "@/components/ui/input";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { toast } from "sonner";
+import Link from "next/link";
 
 export default function LoginPage() {
   const router = useRouter();
@@ -123,6 +124,13 @@ export default function LoginPage() {
             {loading ? "Signing in..." : "Login with Email"}
           </Button>
         </CardContent>
+
+        <p className="text-center text-sm text-muted-foreground pt-2">
+        Don't have an account?{" "}
+        <Link href="/register" className="font-medium text-primary underline-offset-4 hover:underline">
+          Sign up
+        </Link>
+      </p>
       </Card>
     </div>
   );
